41c43723b52a9ffe0f85f54936f51334a60b1d76
contatovinicius.mends <contatovinicius.mends@gmail.com>
2025-08-23 18:48:17 +0000

    ajustando tela cadastro

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const logoFinGo = require('../assets/images/logoFinGo.png');
 
 const WelcomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Welcome Screen</Text>
+      <Image source={logoFinGo} style={styles.logo} />
+      <Text style={styles.title}>Bem-vindo ao FinGo!</Text>
+      <Text style={styles.subtitle}>Aprendendo finanças de forma divertida.</Text>
+
+      <View style={styles.buttonContainer}>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, styles.registerButton]} onPress={() => navigation.navigate('Register')}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -12,7 +29,40 @@ const WelcomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 50,
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: 150, // Ajuste o tamanho conforme necessário
+    height: 150, // Ajuste o tamanho conforme necessário
+    resizeMode: 'contain',
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#007bff', // Exemplo de cor, ajuste conforme seu design
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#666', // Ajuste a cor conforme necessário
+  },
+  buttonContainer: {
+    width: '100%',
     alignItems: 'center',
   },
 });
